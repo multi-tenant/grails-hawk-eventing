99cef6bb7cdaca4749f634b881e2b0a0f935e1cd
Kim A. Betti <kim.betti@gmail.com>
2011-02-11 15:30:30 +0100

    Fixed formating and removed a println

--- src/java/grails/plugins/hawkeventing/config/ConsumerAnnotationReader.java
+++ src/java/grails/plugins/hawkeventing/config/ConsumerAnnotationReader.java
@@ -1,5 +1,10 @@
 package grails.plugins.hawkeventing.config;
 
+import grails.plugins.hawkeventing.EventBroker;
+import grails.plugins.hawkeventing.EventConsumer;
+import grails.plugins.hawkeventing.MethodEventConsumer;
+import grails.plugins.hawkeventing.annotation.Consuming;
+
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,50 +13,47 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.BeansException;
 
-import grails.plugins.hawkeventing.EventBroker;
-import grails.plugins.hawkeventing.EventConsumer;
-import grails.plugins.hawkeventing.MethodEventConsumer;
-import grails.plugins.hawkeventing.annotation.Consuming;
-
 /**
  * Looks at all Spring beans for @Consuming annotations
+ * 
  * @author Kim A. Betti
  */
 public class ConsumerAnnotationReader {
 
-	private static final Log log = LogFactory.getLog(ConsumerAnnotationReader.class);
-	
-	private EventBroker eventBroker;
+    private static final Log log = LogFactory.getLog(ConsumerAnnotationReader.class);
 
-	public void setEventBroker(EventBroker eventBroker) {
-		this.eventBroker = eventBroker;
-	}
+    private EventBroker eventBroker;
 
-	public Object addConsumersFromClass(Object bean, String beanName) throws BeansException {
-		for (Method consumerMethod : getConsumerMethods(bean.getClass())) {
-			EventConsumer eventConsumer = new MethodEventConsumer(bean, consumerMethod);
-			for (String eventName : getEventNames(consumerMethod)) {
-				System.out.println(eventName + " => " + beanName);
-				log.info("Subscribing " + eventName + " to " + beanName + ", method " + consumerMethod.getName());
-				eventBroker.subscribe(eventName, eventConsumer);
-			}
-		}
-		
-		return bean;
-	}
+    public void setEventBroker(EventBroker eventBroker) {
+        this.eventBroker = eventBroker;
+    }
 
-	private Set<Method> getConsumerMethods(Class<?> beanClass) {
-		Set<Method> consumerMethods = new HashSet<Method>();
-		for (Method method : beanClass.getMethods()) 
-			if (method.isAnnotationPresent(Consuming.class)) 
-				consumerMethods.add(method);
-		
-		return consumerMethods;
-	}
-	
-	private String[] getEventNames(Method method) {
-		Consuming consumingAnnotation = method.getAnnotation(Consuming.class);
-		return consumingAnnotation.value();
-	}
-	
+    public Object addConsumersFromClass(Object bean, String beanName) throws BeansException {
+        for (Method consumerMethod : getConsumerMethods(bean.getClass())) {
+            EventConsumer eventConsumer = new MethodEventConsumer(bean, consumerMethod);
+            for (String eventName : getEventNames(consumerMethod)) {
+                log.info("Subscribing " + eventName + " to " + beanName + ", method " + consumerMethod.getName());
+                eventBroker.subscribe(eventName, eventConsumer);
+            }
+        }
+
+        return bean;
+    }
+
+    private Set<Method> getConsumerMethods(Class<?> beanClass) {
+        Set<Method> consumerMethods = new HashSet<Method>();
+        for (Method method : beanClass.getMethods()) {
+            if (method.isAnnotationPresent(Consuming.class)) {
+                consumerMethods.add(method);
+            }
+        }
+
+        return consumerMethods;
+    }
+
+    private String[] getEventNames(Method method) {
+        Consuming consumingAnnotation = method.getAnnotation(Consuming.class);
+        return consumingAnnotation.value();
+    }
+
 }
